5be9e2af073a15079a58355a4c8627843acc3618
julicastro <castroejulian@gmail.com>
2021-09-16 03:01:06 -0300

    arreglos

--- Actividad 2 - Introduccion a PHP/index.php
+++ Actividad 2 - Introduccion a PHP/index.php
@@ -16,6 +16,7 @@ include_once('Ejercicio06/ejercicio06.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="src/style.css">
     <title>Introduccion a PHP</title>
 </head>
 
@@ -23,94 +24,123 @@ include_once('Ejercicio06/ejercicio06.php');
     <header>
         <h1>Julian Castro</h1>
     </header>
+
     <main>
+        <section class="main-content">
+            <div class="ejercicio-01">
+                <h3>Ejercio 01</h3>
+                <p class="consigna">Cree una función llamada Semaforo, que recibe por parametro un cólor cómo texto (“rojo”
+                    “amarillo”,”verde”). Dicha función devolverá el estado que corresponde: “frene”, “precaución”,
+                    “avance” o “estado desconocido” ante un caso no esperado.
+                    a) Resuelva la solución utilizando if else
+                    b) Resuelva la solución utilizando if inline (return ?: )
+                    c) Resuelva la solución utilizando switch
+                </p>
+                <div class="ejercicio-contenedor">
+                    <div>
+                        <?php
+                        ifElse("verde");
+                        ?>
+                    </div>
+                    <div>
+                        <?php
+                        switchFunction("amarillo");
+                        ?>
+                    </div>
+                    <div>
+                        <?php
+                        ifInline("rojo");
+                        ?>
+                    </div>
+                    <div>
+                        <?php
+                        ifInline("messi");
+                        ?>
+                    </div>
+                </div>
+            </div>
 
-    
-    <div class="ejercicio-01">
-        <h3>Ejercio 01</h3>
-        <div>
-            <?php
-            ifElse("verde");
-            ?>
-        </div>
-        <div>
-            <?php
-            switchFunction("amarillo");
-            ?>
-        </div>
-        <div>
-            <?php
-            ifInline("rojo");
-            ?>
-        </div>
-        <div>
-            <?php
-            ifInline("messi");
-            ?>
-        </div>
-    </div>
 
-    <div class="ejercicio-02">
-        <h3>Ejercicio 02</h3>
-        <div>
-            <?php
-            echo binomioCuadradoPerfectoPow($a = 2, $b = 5);
-            ?>
-        </div>
-        <div>
-            <?php
-            echo ejercicioConPow($a = 2, $b = 3);
-            ?>
-        </div>
-    </div>
+            <div class="ejercicio-02">
+                <h3>Ejercicio 02</h3>
 
-    <div class="ejercicio-03">
-        <h3>Ejercicio 03</h3>
-        <div>
-            <?php
-                $t1 = "Hola me llamo julian";
-                $t2 = "y vivo en Haedo";
-                concatenarTextos($t1, $t2);
-            ?>
-        </div>
-    </div>
+                <div class="ejercicio-contenedor">
+                    <div>
+                        <p>
+                            <?php
+                            echo binomioCuadradoPerfectoPow($a = 2, $b = 5);
+                            ?>
+                        </p>
 
-    <div class="ejercicio-04">
-        <h3>Ejercicio 04</h3>
-        <div>
-            <p>Resultados esperados 2 y 3 </p>
-            <?php
-                incrementar(2);
-            ?>
-        </div>
-    </div>
+                    </div>
+                    <div>
+                        <p>
+                            <?php
+                            echo ejercicioConPow($a = 2, $b = 3);
+                            ?>
+                        </p>
 
-    <div class="ejercicio-05">
-        <h3>Ejercicio 05</h3>
-        <div>
-            <?php
-                $sumar = array(1,1,1,1,2);
-                sumatoria($sumar);
-            ?>
-        </div>
-    </div>
+                    </div>
+                </div>
+            </div>
 
-    <div class="ejercicio-06">
-        <h3>Ejercicio 06</h3>
-        <div>
-            <?php
-                saludoFormal("Juan", "Carlos", "m");
-                saludoInformal("Juan");
-            ?>
-        </div>
-    </div>
+            <div class="ejercicio-03">
+                <h3>Ejercicio 03</h3>
+                <div class="ejercicio-contenedor">
+                    <div>
+                        <?php
+                        $t1 = "Hola me llamo julian";
+                        $t2 = "y vivo en Haedo";
+                        concatenarTextos($t1, $t2);
+                        ?>
+                    </div>
+                </div>
+            </div>
+
+            <div class="ejercicio-04">
+                <h3>Ejercicio 04</h3>
+                <div class="ejercicio-contenedor">
+                    <div>
+                        <p>Resultados esperados 2 y 3 </p>
+                        <?php
+                        incrementar(2);
+                        ?>
+                    </div>
+                </div>
+            </div>
+
+            <div class="ejercicio-05">
+                <h3>Ejercicio 05</h3>
+                <div class="ejercicio-contenedor">
+                    <div>
+                        <?php
+                        $sumar = array(1, 1, 1, 1, 2);
+                        sumatoria($sumar);
+                        ?>
+                    </div>
+                </div>
+            </div>
+
+            <div class="ejercicio-06">
+                <h3>Ejercicio 06</h3>
+                <div class="ejercicio-contenedor">
+                    <div>
+                        <?php
+                        saludoFormal("Juan", "Carlos", "m");
+                        saludoInformal("Juan");
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </section>
+
     </main>
 </body>
 
 <!-- div con php -->
 <div>
     <?php
-        
+
     ?>
 </div>
 
